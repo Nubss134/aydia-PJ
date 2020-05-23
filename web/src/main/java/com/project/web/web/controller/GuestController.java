0a65a3577a96296ed6374c6d13c6c32e6b3ba920
Nguyen Dinh Ngoc <ngocnd@rabiloo.com.vn>
2020-05-23 19:49:02 +0700

    update

--- web/src/main/java/com/project/web/web/controller/GuestController.java
+++ web/src/main/java/com/project/web/web/controller/GuestController.java
@@ -1,11 +1,16 @@
 package com.project.web.web.controller;
 
+import com.project.web.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GuestController {
+    @Autowired
+    private NewsService newsService;
 
     @GetMapping("/")
     public String guestIndex() {
@@ -13,12 +18,14 @@ public class GuestController {
     }
 
     @GetMapping("/summary")
-    public String guestDetail() {
+    public String guestDetail(Model model) {
+        model.addAttribute("list",newsService.getListRecent(0L));
         return "guest/detail";
     }
 
     @GetMapping("/detail/news")
-    public String guestNewsDetail(@RequestParam(value="id") Long id) {
+    public String guestNewsDetail(@RequestParam(value="id") Long id, Model model) {
+        model.addAttribute("list",newsService.getListRecent(id));
         return "guest/components/news_detail";
     }
 
